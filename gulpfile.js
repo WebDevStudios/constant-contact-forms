c0b62b023766665d5d8b7ad59dfff13fbe95f378
Damon Cook <damonacook@gmail.com>
2016-06-21 13:37:02 -0400

    set funtion to handle errors with gutil.beep in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,3 +11,21 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sort = require('gulp-sort');
 var sourcemaps = require('gulp-sourcemaps');
+
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors () {
+	var args = Array.prototype.slice.call(arguments);
+
+	notify.onError({
+		title: 'Task Failed [<%= error.message %>',
+		message: 'See console.',
+		sound: 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+	}).apply(this, args);
+
+	gutil.beep(); // Beep 'sosumi' again
+
+	// Prevent the 'watch' task from stopping
+	this.emit('end');
+}
