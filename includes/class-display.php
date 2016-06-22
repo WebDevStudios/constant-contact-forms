833d8ee9a740b2e323dfd2456863673c9806967c
Brad Parbs <brad@bradparbs.com>
2016-06-22 18:40:39 -0500

    Move main form display function to display class, switch to returning data, not echoing

--- includes/class-display.php
+++ includes/class-display.php
@@ -32,6 +32,29 @@ class ConstantContact_Display {
 	}
 
 	/**
+	 * Main wrapper for getting our form display
+	 *
+	 * @author Brad Parbs
+	 * @return string Form markup
+	 */
+	public function form( $form_data ) {
+
+		global $wp;
+
+		$return = '<form id="ctct-form" action="' . esc_url( trailingslashit( add_query_arg( '', '', home_url( $wp->request ) ) ) ) . '" method="post">';
+
+		$return .= ctct_form_submit_message( 'return' );
+		$return .= ctct_build_form_fields( $form_data, 'return' );
+
+		$return .= '<p><input type="submit" name="ctct-submitted" value="' . __( 'Send', 'constantcontact' ) . '"/></p>';
+		$return .= wp_nonce_field( 'ctct_submit_form', 'ctct_form', true, false );
+
+		$return .= '</form>';
+
+		return $return;
+	}
+
+	/**
 	 * Helper method to display form description
 	 *
 	 * @author Brad Parbs
