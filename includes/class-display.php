35be9dc872f3e1cdf602618f62e34e0a870440fb
Brad Parbs <brad@bradparbs.com>
2016-06-22 17:22:31 -0500

    Add docblock, escape description output

--- includes/class-display.php
+++ includes/class-display.php
@@ -31,8 +31,15 @@ class ConstantContact_Display {
 		$this->plugin = $plugin;
 	}
 
+	/**
+	 * Helper method to display form description
+	 *
+	 * @author Brad Parbs
+	 * @param  string $description description to outpu
+	 * @return echo              echos out form description markup
+	 */
 	public function description( $description ) {
-		echo "<p class='constant-contact constant-contact-form-description'>{$description}</p>";
+		echo '<p class="constant-contact constant-contact-form-description">' . esc_attr( $description ) . '</p>';
 	}
 }
 
