4cdf023d965e2ec8bb49c84f111f197f5452cc9f
Brad Parbs <brad@bradparbs.com>
2016-06-22 19:00:45 -0500

    Move build_form_fields, clean up, remove unneeded methods

--- includes/class-display.php
+++ includes/class-display.php
@@ -43,13 +43,66 @@ class ConstantContact_Display {
 
 		$return = '<form id="ctct-form" action="' . esc_url( trailingslashit( add_query_arg( '', '', home_url( $wp->request ) ) ) ) . '" method="post">';
 
-		$return .= ctct_form_submit_message( 'return' );
-		$return .= ctct_build_form_fields( $form_data, 'return' );
+		$return .= constant_contact()->process_form->submit_message( 'return' );
+		$return .= $this->build_form_fields( $form_data );
 
 		$return .= '<p><input type="submit" name="ctct-submitted" value="' . __( 'Send', 'constantcontact' ) . '"/></p>';
 		$return .= wp_nonce_field( 'ctct_submit_form', 'ctct_form', true, false );
 
 		$return .= '</form>';
+
+		return $return;
+	}
+
+	/**
+	 * Build form fields for shortcode
+	 *
+	 * @since 1.0.0
+	 * @param  array $form_data formulated cmb2 data for form.
+	 * @return void
+	 */
+	public function build_form_fields( $form_data ) {
+
+		// start our wrapper return var
+		$return = '';
+
+		// Check to see if we have a description for the form, and display it
+		if (
+			isset( $form_data['options'] ) &&
+			isset( $form_data['options']['description'] ) &&
+			$form_data['options']['description']
+		) {
+			constant_contact()->display->description( esc_attr( $form_data['options']['description'] ) );
+		}
+
+		// Loop through each of our form fields and output it
+		foreach ( $form_data['fields'] as $key => $value ) {
+
+			$required = isset( $form_data['fields'][ $key ]['required'] ) ? ' * required' : '';
+
+			$return .= '<div><p><label>' . esc_attr( $form_data['fields'][ $key ]['name'] ) . esc_attr( $required ) . '</label></br>';
+
+			$field_name = esc_attr( $form_data['fields'][ $key ]['map_to'] );
+			$field_value = ( isset( $_POST[ 'ctct-' . $form_data['fields'][ $key ]['map_to'] ] ) ? esc_attr( $_POST[ 'ctct-' . $form_data['fields'][ $key ]['map_to'] ] ) : '' );
+
+			switch ( $form_data['fields'][ $key ]['map_to'] ) {
+
+				case 'email':
+						$return .= '<input type="email" required name="ctct-' . esc_attr( $field_name ) . '" value="' . esc_attr( $field_value ) . '" tabindex="1" size="40"></p></div>';
+				break;
+				default:
+						$return .= '<input type="text" pattern="[a-zA-Z0-9 ]+" name="ctct-' . esc_attr( $field_name ) . '" value="' . esc_attr( $field_value ) . '" tabindex="1" size="40"></p></div>';
+				break;
+
+			}
+		}
+
+		if ( isset( $form_data['options']['opt_in'] ) && isset( $form_data['options']['list'] ) ) {
+				$return .= '<div><p>';
+					$return .= '<input type="checkbox" name="ctct-opti-in" value="' . esc_attr( $form_data['options']['list'] ) . '"/>';
+					$return .= esc_attr( $form_data['options']['opt_in'] );
+				$return .= '</p></div>';
+		}
 
 		return $return;
 	}
--- includes/class-process-form.php
+++ includes/class-process-form.php
@@ -105,96 +105,34 @@ class ConstantContact_Process_Form {
 		}
 	}
 
-/**
- * Form submit success/error messages.
- *
- * @since 1.0.0
- * @return void
- */
-function ctct_form_submit_message( $mode = 'echo ') {
+	/**
+	 * Form submit success/error messages.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
+	public function submit_message( $mode = 'echo ') {
 
-	if ( $message = get_transient( 'ctct_form_submit_message' ) ) {
+		if ( $message = get_transient( 'ctct_form_submit_message' ) ) {
 
-		switch ( $message ) {
-			case 'success':
-				$message_text = __( 'Your message has been sent!', 'constantcontact' );
-			break;
-			case 'error':
-				$message_text = __( 'Your message failed to send!', 'constantcontact' );
-			break;
-		}
+			switch ( $message ) {
+				case 'success':
+					$message_text = __( 'Your message has been sent!', 'constantcontact' );
+				break;
+				case 'error':
+					$message_text = __( 'Your message failed to send!', 'constantcontact' );
+				break;
+			}
 
-		$return = sprintf( '<p class="message ' . esc_attr( $message ) . '"> %s </p>', esc_attr( $message_text ) );
+			$return = sprintf( '<p class="message ' . esc_attr( $message ) . '"> %s </p>', esc_attr( $message_text ) );
 
-		delete_transient( 'ctct_form_submit_message' );
+			delete_transient( 'ctct_form_submit_message' );
 
-		if ( 'echo' == $mode ) {
-			echo $return;
-		} else {
-			return $return;
-		}
-	}
-}
-
-/**
- * Build form fields for shortcode
- *
- * @since 1.0.0
- * @param  array $form_data formulated cmb2 data for form.
- * @return void
- */
-function ctct_build_form_fields( $form_data, $display_mode = 'echo' ) {
-
-	// start our wrapper return var
-	$return = '';
-
-	// Check to see if we have a description for the form, and display it
-	if (
-		isset( $form_data['options'] ) &&
-		isset( $form_data['options']['description'] ) &&
-		$form_data['options']['description']
-	) {
-		constant_contact()->display->description( esc_attr( $form_data['options']['description'] ) );
-	}
-
-	// Loop through each of our form fields and output it
-	foreach ( $form_data['fields'] as $key => $value ) {
-
-		$required = isset( $form_data['fields'][ $key ]['required'] ) ? ' * required' : '';
-
-		$return .= '<div><p><label>' . esc_attr( $form_data['fields'][ $key ]['name'] ) . esc_attr( $required ) . '</label></br>';
-
-		$field_name = esc_attr( $form_data['fields'][ $key ]['map_to'] );
-		$field_value = ( isset( $_POST[ 'ctct-' . $form_data['fields'][ $key ]['map_to'] ] ) ? esc_attr( $_POST[ 'ctct-' . $form_data['fields'][ $key ]['map_to'] ] ) : '' );
-
-		switch ( $form_data['fields'][ $key ]['map_to'] ) {
-
-			case 'email':
-					$return .= '<input type="email" required name="ctct-' . esc_attr( $field_name ) . '" value="' . esc_attr( $field_value ) . '" tabindex="1" size="40"></p></div>';
-			break;
-			default:
-					$return .= '<input type="text" pattern="[a-zA-Z0-9 ]+" name="ctct-' . esc_attr( $field_name ) . '" value="' . esc_attr( $field_value ) . '" tabindex="1" size="40"></p></div>';
-			break;
-
+			if ( 'echo' == $mode ) {
+				echo $return;
+			} else {
+				return $return;
+			}
 		}
 	}
-
-	if ( isset( $form_data['options']['opt_in'] ) && isset( $form_data['options']['list'] ) ) {
-			$return .= '<div><p>';
-				$return .= '<input type="checkbox" name="ctct-opti-in" value="' . esc_attr( $form_data['options']['list'] ) . '"/>';
-				$return .= esc_attr( $form_data['options']['opt_in'] );
-			$return .= '</p></div>';
-	}
-
-	if ( 'echo' == $display_mode ) {
-		echo $return;
-	} else {
-		return $return;
-	}
-
-}
-
-
-function ctct_validate_form_fields( $form_data ) {
-	return true;
 }
