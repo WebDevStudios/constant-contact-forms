bcd489a6eb268e756205045a698b9c80c3753418
Brad Parbs <brad@bradparbs.com>
2016-06-27 16:54:39 -0500

    Spacing clean up for ctct-support.js

--- assets/js/ctct-plugin-admin/ctct-support.js
+++ assets/js/ctct-plugin-admin/ctct-support.js
@@ -1,38 +1,38 @@
 window.CTCTSupport = {};
-	( function( window, $, that ) {
+( function( window, $, that ) {
 
-		// Constructor.
-		that.init = function() {
-			that.cache();
-			that.bindEvents();
-		}
+	// Constructor.
+	that.init = function() {
+		that.cache();
+		that.bindEvents();
+	}
 
-		// Cache all the things.
-		that.cache = function() {
-			that.$c = {
-				window: $( window ),
-				body: $( 'body' ),
-				hide: '.answer',
-			};
-		}
+	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $( window ),
+			body: $( 'body' ),
+			hide: '.answer',
+		};
+	}
 
-		// Combine all events.
-		that.bindEvents = function() {
-			$( that.$c.hide ).hide();
-            // Show fields based on selection
-            $('.question').on( 'click', function(e) {
-				that.metaShowHide( $(this).next('.answer') );
-			});
-        }
+	// Combine all events.
+	that.bindEvents = function() {
+		$( that.$c.hide ).hide();
+        // Show fields based on selection
+        $('.question').on( 'click', function(e) {
+			that.metaShowHide( $(this).next('.answer') );
+		});
+    }
 
-        // Function to handle which items should be showing/hiding
-        that.metaShowHide = function(showem) {
-            var hideThese = $( that.$c.hide ).not(showem);
-            showem.slideDown('fast');
-            hideThese.hide();
-        }
+    // Function to handle which items should be showing/hiding
+    that.metaShowHide = function(showem) {
+        var hideThese = $( that.$c.hide ).not(showem);
+        showem.slideDown('fast');
+        hideThese.hide();
+    }
 
-		// Engage!
-		$( that.init );
+	// Engage!
+	$( that.init );
 
-	})( window, jQuery, window.CTCTSupport );
+})( window, jQuery, window.CTCTSupport );
