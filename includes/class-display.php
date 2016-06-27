9083158aff058d486da4cbfdbaedbba8711c0c1f
Damon Cook <damonacook@gmail.com>
2016-06-27 16:31:36 -0400

    enqueue_script( 'ctct_frontend_forms' ); in class-display.php

--- includes/class-display.php
+++ includes/class-display.php
@@ -71,10 +71,12 @@ class ConstantContact_Display {
 				array(),
 				constant_contact()->version
 			);
-			
-			// Enqueued script.
-			wp_enqueue_script( 'ctct_frontend_forms' );
 		}
+		
+		$this->scripts();
+		
+		// Enqueued script.
+		wp_enqueue_script( 'ctct_frontend_forms' );
 
 		// Start our return markup and some default variables
 		$return           = '';
