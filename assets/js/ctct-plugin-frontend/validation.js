de3092558b5495c39da93bc3f3947d803db0f9e9
Brad Parbs <brad@bradparbs.com>
2016-06-27 16:54:29 -0500

    Start adding validation to form submission front-end

--- assets/js/ctct-plugin-frontend/validation.js
+++ assets/js/ctct-plugin-frontend/validation.js
@@ -1,1 +1,43 @@
-var $test = "this is a test";
+window.CTCTSupport = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		that.cache();
+		that.bindEvents();
+	}
+
+	// Cache all the things.
+	that.cache = function() {
+		that.$c = {
+			window: $( window ),
+			body: $( 'body' ),
+			form: '.ctct-form-wrapper form',
+		};
+		that.timeout = null;
+	}
+
+	// Combine all events.
+	that.bindEvents = function() {
+		$( that.$c.form + ' input' ).keyup( function() {
+			clearTimeout( that.timeout )
+			that.timeout = setTimeout( function() {
+				$.post(
+				    ajaxurl,
+				    {
+				        'action': 'ctct_process_form',
+				        'data':   $( that.$c.form ).serialize(),
+				    },
+				    function(response){
+				        console.log( response );
+				    }
+				);
+			}, 500 )
+		})
+    }
+
+
+	// Engage!
+	$( that.init );
+
+})( window, jQuery, window.CTCTSupport );
