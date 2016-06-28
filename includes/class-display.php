441fb446a85cbaf6bc27642c76cd471921c87101
Brad Parbs <brad@bradparbs.com>
2016-06-27 21:48:26 -0500

    Allow passing server-side errors to the front end

--- includes/class-display.php
+++ includes/class-display.php
@@ -272,6 +272,38 @@ class ConstantContact_Display {
 			$map = $map . '___' . md5( serialize( $field ) );
 		}
 
+		// Default error status
+		$field_error = false;
+
+		// If we got any errors, then pass them through to the form field
+		if ( ! empty( $req_errors ) ) {
+
+			// Loop through each error
+			foreach ( $req_errors as $error ) {
+
+				// Make sure we have a field ID and an actual error
+				if ( isset( $error['id'] ) && isset( $error['error'] ) ) {
+
+					// If the error matches the field we're rendering
+					if ( $map == $error['id'] ) {
+
+						// Start our field error return
+						$field_error = '<span class="ctct-field-error">';
+
+						// Based on the error type, display an error.
+						if ( 'invalid' == $error['error'] ) {
+							 $field_error .= __( 'Error: Please correct your entry.', 'constantcontact' );
+						} else {
+							$field_error .= __( ' Error: Please fill out this field.', 'constantcontact' );
+						}
+
+						// Finish error return
+						$field_error .= '</span>';
+					}
+				}
+			}
+		}
+
 		// Potentially replace value with submitted value
 		$value = $this->get_submitted_value( $value, $map, $field, $old_values );
 
@@ -285,10 +317,10 @@ class ConstantContact_Display {
 			case 'company':
 			case 'website':
 			case 'text_field':
-				return $this->input( 'text', $name, $map, $value, $desc, $req );
+				return $this->input( 'text', $name, $map, $value, $desc, $req, false, $field_error );
 				break;
 			case 'email':
-				return $this->input( 'email', $name, $map, $value, $desc, $req );
+				return $this->input( 'email', $name, $map, $value, $desc, $req, false, $field_error );
 				break;
 			case 'hidden':
 				return $this->input( 'hidden', $name, $map, $value, $desc, $req );
@@ -297,18 +329,18 @@ class ConstantContact_Display {
 				return $this->checkbox( $name, $map, $value, $desc );
 				break;
 			case 'submit':
-				return $this->input( 'submit', $name, $map, $value, $desc );
+				return $this->input( 'submit', $name, $map, $value, $desc, $req, false, $field_error );
 				break;
 			case 'address':
-				return $this->address( $name, $map, $value, $desc );
+				return $this->address( $name, $map, $value, $desc, $req, $field_error );
 				break;
 			case 'anniversery':
 			case 'birthday':
 				// need this to be month / day / year
-				return $this->input( 'text', $name, $map, $value, $desc );
+				return $this->input( 'text', $name, $map, $value, $desc, $req, false, $field_error );
 				break;
 			default:
-				return $this->input( 'text', $name, $map, $value, $desc, $req );
+				return $this->input( 'text', $name, $map, $value, $desc, $req, false, $field_error );
 				break;
 		}
 	}
@@ -458,7 +490,7 @@ class ConstantContact_Display {
 	 * @param  boolean $f_only should we only return the field itself, with no label?
 	 * @return string          HTML markup for field
 	 */
-	public function input( $type = 'text', $name = '', $id = '', $value = '', $label = '', $req = false, $f_only = false ) {
+	public function input( $type = 'text', $name = '', $id = '', $value = '', $label = '', $req = false, $f_only = false, $field_error = false ) {
 
 		// Sanitize our stuff / set values
 		$name  = sanitize_text_field( $name );
@@ -472,11 +504,23 @@ class ConstantContact_Display {
 		$markup = $this->field_top( $type, $name, $f_id, $label, $req );
 
 		// Set our field as as seprate var, because we allow for only returning that
-		$field = '<input ' . $req_text . ' type="' . $type . '" name="' . $f_id . '" id="' . $f_id . '" value="' . $value . '" />';
+		$field = '<input ' . $req_text . ' type="' . $type . '" name="' . $f_id . '" id="' . $f_id . '" value="' . $value . '"/>';
+
+		// If we have an error
+		if ( $field_error ) {
+			// Tack that sucker on to the end of our input
+			$field = str_replace( '/>', 'class="ctct-invalid />', $field );
+		}
 
 		// Add our field to our markup
 		$markup .= $field;
-		$markup .= $this->field_bottom();
+
+		// If we got an error, add it to the bottom label
+		if ( $field_error ) {
+			$markup .= $this->field_bottom( $id, $field_error );
+		} else {
+			$markup .= $this->field_bottom();
+		}
 
 		// If we passed in a flag for only the field, just return that
 		if ( $f_only ) {
@@ -557,7 +601,7 @@ class ConstantContact_Display {
 	 * @param  string $desc  label of field
 	 * @return string        html markup of field
 	 */
-	public function address( $name = '', $f_id = '', $value = array(), $desc = '' ) {
+	public function address( $name = '', $f_id = '', $value = array(), $desc = '', $req = false, $field_error = '' ) {
 
 		// Set up our text strings
 		$street = __( 'Street Address', 'constantcontact' );
