1c9603c61c1ca8f2acb6a200bb8a890b2084fa9f
Brad Parbs <brad@bradparbs.com>
2016-08-23 10:25:24 -0500

    Force https for middlware

--- includes/class-middleware.php
+++ includes/class-middleware.php
@@ -73,7 +73,7 @@ class ConstantContact_Middleware {
 	 * @return string url of auth server base
 	 */
 	public function get_auth_server_link() {
-		return 'http://wpredirect.constantcontact.com/';
+		return 'https://wpredirect.constantcontact.com/';
 	}
 
 	/**
