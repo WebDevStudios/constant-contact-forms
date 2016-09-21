696c63878b2781a2867d7b070d3414bf06ca7a3d
Michael Beckwith <tw2113@gmail.com>
2016-09-21 13:36:52 -0500

    remove assignment inside empty() function for 5.4 compatibility

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -245,7 +245,8 @@ class ConstantContact_Settings {
 			$business_addr = '';
 
 			// We might be able to get it from the API?
-			if ( ! empty( $disclosure_info = $this->plugin->api->get_disclosure_info( true ) ) ) {
+			$disclosure_info = $this->plugin->api->get_disclosure_info( true );
+			if ( ! empty( $disclosure_info ) ) {
 				// Make sure no one can edit.
 				$business_name = $disclosure_info['name']    ?: $business_name;
 				$business_addr = $disclosure_info['address'] ?: '';
