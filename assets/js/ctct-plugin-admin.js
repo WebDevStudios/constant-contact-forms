8edbf957075c23700c6d69f0b1a8cfa5820d26d1
Michael Beckwith <tw2113@gmail.com>
2017-01-04 19:53:54 -0600

    new js for ajax

--- assets/js/ctct-plugin-admin.js
+++ assets/js/ctct-plugin-admin.js
@@ -1,3 +1,22 @@
+window.CTCTAJAX = {};
+( function( window, $, that ) {
+
+	// Constructor.
+	that.init = function() {
+		// Trigger any field modifications we need to do
+		that.handleOptinAJAX();
+	}
+
+	// We need to manipulate our form builder a bit. We do this here.
+	that.handleOptinAJAX = function() {
+
+	};
+
+	// Engage!
+	$( that.init );
+
+})( window, jQuery, window.CTCTAJAX );
+
 window.CTCTBuilder = {};
 ( function( window, $, that ) {
 
