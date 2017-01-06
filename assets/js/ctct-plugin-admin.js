39a015dea0c6431e856219e889602583df721e3c
Michael Beckwith <tw2113@gmail.com>
2017-01-06 14:57:49 -0600

    commit our compiled styles and js

--- assets/js/ctct-plugin-admin.js
+++ assets/js/ctct-plugin-admin.js
@@ -9,7 +9,26 @@ window.CTCTAJAX = {};
 
 	// We need to manipulate our form builder a bit. We do this here.
 	that.handleOptinAJAX = function() {
+		$('#ctct_admin_notice_tracking_optin').on('click',function(e){
+			var ctct_optin_ajax = {
+				'action': 'constant_contact_optin_ajax_handler',
+				'optin': ($(this).is(':checked')) ? 'on' : 'off'
+			}
 
+			$.ajax({
+				url     : ajaxurl,
+				data    : ctct_optin_ajax,
+				dataType: 'json',
+				success : function (response) {
+					console.log(response);
+				},
+				error: function(x, t, m){
+					if (window.console) {
+						console.log([t, m]);
+					}
+				}
+			});
+		});
 	};
 
 	// Engage!
