e1654ce5dc13aa847634bbe98a58cc00441dbab4
Michael Beckwith <tw2113@gmail.com>
2017-01-13 10:34:29 -0600

    add trademark symbol next to our two instances of Google Analytics

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -246,10 +246,10 @@ class ConstantContact_Settings {
 	public function do_lists_field( $cmb ) {
 
 		$cmb->add_field( array(
-			'name' => __( 'Google Analytics tracking opt-in.', 'constant-contact-forms' ),
+			'name' => __( 'Google Analytics&trade; tracking opt-in.', 'constant-contact-forms' ),
 			'id'   => '_ctct_data_tracking',
 			'type' => 'checkbox',
-			'desc' => __( 'Allow Constant Contact to use Google Analytics to track your usage across the Constant Contact Forms plugin.<br/> NOTE &mdash; Your website and users will not be tracked. See our <a href="https://www.constantcontact.com/legal/privacy-statement"> Privacy Statement</a> information about what is and is not tracked.', 'constant-contact-forms' ),
+			'desc' => __( 'Allow Constant Contact to use Google Analytics&trade; to track your usage across the Constant Contact Forms plugin.<br/> NOTE &mdash; Your website and users will not be tracked. See our <a href="https://www.constantcontact.com/legal/privacy-statement"> Privacy Statement</a> information about what is and is not tracked.', 'constant-contact-forms' ),
 		) );
 
 		// Only show our settings page if we're connected to CC.
