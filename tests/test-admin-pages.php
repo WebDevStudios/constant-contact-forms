fb8ac17a1d153a3a1650196eb3f3c1a41a870c76
Michael Beckwith <tw2113@gmail.com>
2017-05-08 23:52:49 -0500

    remove admin check

--- tests/test-admin-pages.php
+++ tests/test-admin-pages.php
@@ -14,9 +14,7 @@ class ConstantContact_Admin_Pages_Test extends WP_UnitTestCase {
 
 	function test_class_access() {
 
-		if ( is_admin() ) {
-			$this->assertTrue( constant_contact()->admin_pages instanceof ConstantContact_Admin_Pages );
-		}
+		$this->assertTrue( constant_contact()->admin_pages instanceof ConstantContact_Admin_Pages );
 	}
 
 	function test_sample() {
--- tests/test-admin.php
+++ tests/test-admin.php
@@ -14,9 +14,7 @@ class ConstantContact_Admin_Test extends WP_UnitTestCase {
 
 	function test_class_access() {
 
-		if ( is_admin() ) {
-			$this->assertTrue( constant_contact()->admin instanceof ConstantContact_Admin );
-		}
+		$this->assertTrue( constant_contact()->admin instanceof ConstantContact_Admin );
 	}
 
 	function test_sample() {
