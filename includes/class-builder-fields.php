12eb06ae3383a58b0b38ecfedcbbfd700bff8b61
Michael Beckwith <tw2113@gmail.com>
2018-07-27 10:30:06 -0500

    move from margins on each field type, to padding on the whole form

--- includes/class-builder-fields.php
+++ includes/class-builder-fields.php
@@ -300,17 +300,17 @@ class ConstantContact_Builder_Fields {
 		) );
 
 		$custom_css_metabox->add_field( array(
-			'name'        => esc_html__( 'Form Input Margins', 'constant-contact-forms' ),
+			'name'        => esc_html__( 'Form Padding', 'constant-contact-forms' ),
 			'type'        => 'title',
-			'id'          => 'form-margin-title',
+			'id'          => 'form-padding-title',
 			'description' => esc_html__(
-				'Enter margins in number of pixels. Margins will be applied to all inputs in the form.',
+				'Enter padding values in number of pixels. Padding will be applied to four sides of the form.',
 				'constant-contact-form' ),
 		) );
 
 		$custom_css_metabox->add_field( array(
 			'name'       => __( 'Top', 'constant-contact-forms' ),
-			'id'         => $this->prefix . 'input_margin_top',
+			'id'         => $this->prefix . 'form_padding_top',
 			'type'       => 'text_small',
 			'show_names' => true,
 			'attributes' => array(
@@ -320,7 +320,7 @@ class ConstantContact_Builder_Fields {
 
 		$custom_css_metabox->add_field( array(
 			'name'       => __( 'Right', 'constant-contact-forms' ),
-			'id'         => $this->prefix . 'input_margin_right',
+			'id'         => $this->prefix . 'form_padding_right',
 			'type'       => 'text_small',
 			'show_names' => true,
 			'attributes' => array(
@@ -330,7 +330,7 @@ class ConstantContact_Builder_Fields {
 
 		$custom_css_metabox->add_field( array(
 			'name'       => __( 'Bottom', 'constant-contact-forms' ),
-			'id'         => $this->prefix . 'input_margin_bottom',
+			'id'         => $this->prefix . 'form_padding_bottom',
 			'type'       => 'text_small',
 			'show_names' => true,
 			'attributes' => array(
@@ -340,7 +340,7 @@ class ConstantContact_Builder_Fields {
 
 		$custom_css_metabox->add_field( array(
 			'name'       => __( 'Left', 'constant-contact-forms' ),
-			'id'         => $this->prefix . 'input_margin_left',
+			'id'         => $this->prefix . 'form_padding_left',
 			'type'       => 'text_small',
 			'show_names' => true,
 			'attributes' => array(
