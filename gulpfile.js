040cca8a09d5aa5a14cbd7121a98adbcdcaec3e8
Eric Fuller <info@glassmoonmedia.com>
2019-02-07 10:19:34 -0500

    Adjust the gulp file so that watch will reload on a change to the gutenberg scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,7 @@ function getFolders(dir) {
  * Use webpack to transpile and bundle scripts.
  */
 gulp.task('scripts', function(done) {
-	gulp.src( [ './assets/js/ctct-plugin-frontend/index.js', './assets/js/ctct-plugin-admin/index.js' ] )
+	gulp.src( [ './assets/js/ctct-plugin-frontend/index.js', './assets/js/ctct-plugin-gutenberg/index.js', './assets/js/ctct-plugin-admin/index.js' ] )
 		.pipe( webpackStream( webpackConfig ), webpack )
 		.pipe( gulp.dest( './assets/js' ) );
 	done();
@@ -153,9 +153,11 @@ gulp.task('watch', function() {
 
 	gulp.watch('./assets/sass/**/*.scss', gulp.series('cssnano'));
 	gulp.watch( './assets/js/ctct-plugin-admin/**/*.js', gulp.series('scripts'));
+	gulp.watch( './assets/js/ctct-plugin-gutenberg/**/*.js', gulp.series('scripts'));
 	gulp.watch( './assets/js/ctct-plugin-frontend/**/*.js', gulp.series('scripts'));
 	gulp.watch( './assets/sass/**/*.scss' ).on( 'change', browserSync.reload );
-	gulp.watch( './assets/js/ctct-plugin-admin.js' ).on( 'change', browserSync.reload );
+	gulp.watch( './assets/js/ctct-plugin-admin/**/*.js' ).on( 'change', browserSync.reload );
+	gulp.watch( './assets/js/ctct-plugin-gutenberg/**/*.js' ).on( 'change', browserSync.reload );
 	gulp.watch( './assets/js/ctct-plugin-frontend/**/*.js' ).on( 'change', browserSync.reload );
 });
 
