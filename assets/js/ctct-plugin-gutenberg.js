95ef2ccf47f7fa9bea4530e8410bb09c2d7e72fa
Eric Fuller <info@glassmoonmedia.com>
2019-02-12 13:49:51 -0500

    Compile chores.

--- assets/js/ctct-plugin-gutenberg.js
+++ assets/js/ctct-plugin-gutenberg.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:3000/wp-content/plugins/constant-contact-forms/assets/js/";
+/******/ 	__webpack_require__.p = "https://localhost:3000/wp-content/plugins/constant-contact-forms/assets/js/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
@@ -86,14 +86,27 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./assets/js/ctct-plugin-gutenberg/blocks/single-contact-form.js":
+/*!***********************************************************************!*\
+  !*** ./assets/js/ctct-plugin-gutenberg/blocks/single-contact-form.js ***!
+  \***********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar __ = wp.i18n.__;\nvar registerBlockType = wp.blocks.registerBlockType;\n/* harmony default export */ __webpack_exports__[\"default\"] = (registerBlockType('constant-contact/single-contact-form', {\n  title: __('Constant Contact: Single Form', 'constant-contact'),\n  icon: 'index-card',\n  category: 'layout',\n  edit: function edit() {\n    return React.createElement(\"div\", null, React.createElement(\"h1\", null, __('Hello, from EDIT', 'constant-contact')));\n  },\n  save: function save() {\n    return null;\n  }\n}));//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hc3NldHMvanMvY3RjdC1wbHVnaW4tZ3V0ZW5iZXJnL2Jsb2Nrcy9zaW5nbGUtY29udGFjdC1mb3JtLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2N0Y3QtcGx1Z2luLWd1dGVuYmVyZy9ibG9ja3Mvc2luZ2xlLWNvbnRhY3QtZm9ybS5qcz85MjExIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IHsgX18gfSA9IHdwLmkxOG47XG5jb25zdCB7XG5cdHJlZ2lzdGVyQmxvY2tUeXBlLFxufSA9IHdwLmJsb2NrcztcblxuZXhwb3J0IGRlZmF1bHQgcmVnaXN0ZXJCbG9ja1R5cGUoICdjb25zdGFudC1jb250YWN0L3NpbmdsZS1jb250YWN0LWZvcm0nLCB7XG5cdHRpdGxlOiBfXyggJ0NvbnN0YW50IENvbnRhY3Q6IFNpbmdsZSBGb3JtJywgJ2NvbnN0YW50LWNvbnRhY3QnICksXG5cdGljb246ICdpbmRleC1jYXJkJyxcblx0Y2F0ZWdvcnk6ICdsYXlvdXQnLFxuXHRlZGl0OiAoKSA9PiB7XG5cdFx0cmV0dXJuIChcblx0XHRcdDxkaXY+XG5cdFx0XHRcdDxoMT57IF9fKCAnSGVsbG8sIGZyb20gRURJVCcsICdjb25zdGFudC1jb250YWN0JyApIH08L2gxPlxuXHRcdFx0PC9kaXY+XG5cdFx0KVxuXHR9LFxuXHRzYXZlOiAoKSA9PiBudWxsXG59KTtcbiJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUVBO0FBR0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBS0E7QUFDQTtBQUFBO0FBQUE7QUFYQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./assets/js/ctct-plugin-gutenberg/blocks/single-contact-form.js\n");
+
+/***/ }),
+
 /***/ "./assets/js/ctct-plugin-gutenberg/index.js":
 /*!**************************************************!*\
   !*** ./assets/js/ctct-plugin-gutenberg/index.js ***!
   \**************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("// This is the entry point for Gutenberg JS.\n// Add JavaScript imports here.//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hc3NldHMvanMvY3RjdC1wbHVnaW4tZ3V0ZW5iZXJnL2luZGV4LmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2N0Y3QtcGx1Z2luLWd1dGVuYmVyZy9pbmRleC5qcz9lMzYwIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIFRoaXMgaXMgdGhlIGVudHJ5IHBvaW50IGZvciBHdXRlbmJlcmcgSlMuXG4vLyBBZGQgSmF2YVNjcmlwdCBpbXBvcnRzIGhlcmUuXG4iXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0EiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./assets/js/ctct-plugin-gutenberg/index.js\n");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _blocks_single_contact_form__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./blocks/single-contact-form */ \"./assets/js/ctct-plugin-gutenberg/blocks/single-contact-form.js\");\n// This is the entry point for Gutenberg JS.\n// Add JavaScript imports here.\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9hc3NldHMvanMvY3RjdC1wbHVnaW4tZ3V0ZW5iZXJnL2luZGV4LmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vYXNzZXRzL2pzL2N0Y3QtcGx1Z2luLWd1dGVuYmVyZy9pbmRleC5qcz9lMzYwIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIFRoaXMgaXMgdGhlIGVudHJ5IHBvaW50IGZvciBHdXRlbmJlcmcgSlMuXG4vLyBBZGQgSmF2YVNjcmlwdCBpbXBvcnRzIGhlcmUuXG5pbXBvcnQgJy4vYmxvY2tzL3NpbmdsZS1jb250YWN0LWZvcm0nOyJdLCJtYXBwaW5ncyI6IkFBQUE7QUFBQTtBQUFBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./assets/js/ctct-plugin-gutenberg/index.js\n");
 
 /***/ }),
 
