3c7ca2c7884235b5d315f646dc58d44d9a4ee9de
Eric Fuller <info@glassmoonmedia.com>
2019-02-19 12:33:43 -0500

    Add in and use i18n function / Add in a default 'Select a form' option.

--- assets/js/ctct-plugin-gutenberg/components/single-form-select.js
+++ assets/js/ctct-plugin-gutenberg/components/single-form-select.js
@@ -2,13 +2,16 @@ import { SelectControl } from '@wordpress/components';
 import apiFetch from '@wordpress/api-fetch';
 
 const { Component } = wp.element;
+const { __ } = wp.i18n;
 
 class SingleFormSelect extends Component {
 	constructor( props ) {
 		super( props );
 
 		this.state = {
-			forms: []
+			forms: [
+				{ label: __( 'Select a form' ), value: 0 }
+			]
 		}
 	}
 
@@ -17,20 +20,20 @@ class SingleFormSelect extends Component {
 		try {
 			const results = await apiFetch( { path: '/wp-json/wp/v2/ctct_forms' } );
 			const forms = results.map( result => ( { label: result.title.rendered, value: result.id } ) );
-			this.setState( { forms } );
+			this.setState( { forms: [...this.state.forms, ...forms ] } );
 		} catch ( e ) {
 			console.error('ERROR: ', e.message );
 		}
 	}
 
 	render() {
-		const { selectedForm } = this.props.attributes;
+		let { selectedForm } = this.props.attributes;
+		selectedForm = undefined !== selectedForm ? selectedForm : __( 'Select a form' );
 
 		return <SelectControl
-			value={ selectedForm }
-			options={ this.state.forms }
-			onChange={ value => this.props.setAttributes( { selectedForm: value })}
-		/>
+				value={ selectedForm }
+				options={ this.state.forms }
+				onChange={ value => this.props.setAttributes( { selectedForm: value } ) } />
 	}
 }
 
