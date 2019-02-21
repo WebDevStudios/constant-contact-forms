1c3eedb61d49f0da1cedf167d003e22cee416f69
Michael Beckwith <tw2113@gmail.com>
2019-02-20 23:01:12 -0600

    update a missed link for the old privacy policy destination.

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -251,7 +251,7 @@ class ConstantContact_Settings {
 			'name' => esc_html__( 'Google Analytics&trade; tracking opt-in.', 'constant-contact-forms' ),
 			'id'   => '_ctct_data_tracking',
 			'type' => 'checkbox',
-			'desc' => __( 'Allow Constant Contact to use Google Analytics&trade; to track your usage across the Constant Contact Forms plugin.<br/> NOTE &mdash; Your website and users will not be tracked. See our <a href="https://www.constantcontact.com/legal/privacy-statement"> Privacy Statement</a> information about what is and is not tracked.', 'constant-contact-forms' ),
+			'desc' => __( 'Allow Constant Contact to use Google Analytics&trade; to track your usage across the Constant Contact Forms plugin.<br/> NOTE &mdash; Your website and users will not be tracked. See our <a href="https://www.endurance.com/privacy"> Privacy Statement</a> information about what is and is not tracked.', 'constant-contact-forms' ),
 		) );
 
 		// Only show our settings page if we're connected to CC.
