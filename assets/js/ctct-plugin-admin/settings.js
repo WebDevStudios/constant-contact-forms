580c45516688f2f7e5a3e11ee786623ea107fd80
George Gecewicz <gecewicz.george@gmail.com>
2019-06-20 12:42:09 -0400

    fix some settings.js spacing

--- assets/js/ctct-plugin-admin/settings.js
+++ assets/js/ctct-plugin-admin/settings.js
@@ -9,25 +9,25 @@ window.CTCTSettings = {};
 	 * @since 1.6.0
 	 */
 	app.init = () => {
-        app.initTooltips();
-    };
+		app.initTooltips();
+	};
 
-    /**
+	/**
 	 * Steps tooltip auto-hiding on mouseenter, closes tooltip on mouseleave.
 	 *
 	 * @author Constant Contact
 	 * @since 1.6.0
 	 */
-    app.allowMouseHoverOnTooltips = () => {
+	app.allowMouseHoverOnTooltips = () => {
 
-        $( document ).on( 'mouseenter', '.ctct-options-ui-tooltip', ( e ) => {
+		$( document ).on( 'mouseenter', '.ctct-options-ui-tooltip', ( e ) => {
 			clearTimeout( window.CTCTCloseTooltipTimer );
-        } );
+		} );
 
-        $( document ).on( 'mouseleave', '.ctct-options-ui-tooltip', ( e ) => {
-            $( '.ctct-options-tooltip' ).tooltip( 'close' );
-        } );
-    };
+		$( document ).on( 'mouseleave', '.ctct-options-ui-tooltip', ( e ) => {
+			$( '.ctct-options-tooltip' ).tooltip( 'close' );
+		} );
+	};
 
 	/**
 	 * Inits jQuery UI tooltips.
@@ -35,41 +35,41 @@ window.CTCTSettings = {};
 	 * @author Constant Contact
 	 * @since 1.6.0
 	 */
-    app.initTooltips = () => {
+	app.initTooltips = () => {
 
-        $( '.ctct-options-tooltip' ).each( ( i, el ) => {
-            let $tooltip = $( el );
+		$( '.ctct-options-tooltip' ).each( ( i, el ) => {
+			let $tooltip = $( el );
 
-            let settings = {
-                hide: { duration: 200 },
-                show: { duration: 200 },
-                tooltipClass: 'ctct-options-ui-tooltip',
-                position: {
-                    my: 'center top',
-                    at: 'center bottom',
-                    collision: 'flipfit'
-                },
-                content: () => {
-                    return $tooltip.prop( 'title' );
-                },
-                open: () => {
-                    $( '.ctct-options-tooltip' ).not( $tooltip ).tooltip( 'close' );
-                }
-            };
+			let settings = {
+				hide: { duration: 200 },
+				show: { duration: 200 },
+				tooltipClass: 'ctct-options-ui-tooltip',
+				position: {
+					my: 'center top',
+					at: 'center bottom',
+					collision: 'flipfit'
+				},
+				content: () => {
+					return $tooltip.prop( 'title' );
+				},
+				open: () => {
+					$( '.ctct-options-tooltip' ).not( $tooltip ).tooltip( 'close' );
+				}
+			};
 
-            // Basic tooltip init, with simple callback that sets up app.allowMouseHoverOnTooltips() functionality.
-            $tooltip.tooltip( settings ).on( 'mouseleave', ( e ) => {
+			// Basic tooltip init, with simple callback that sets up app.allowMouseHoverOnTooltips() functionality.
+			$tooltip.tooltip( settings ).on( 'mouseleave', ( e ) => {
 
-                window.CTCTCloseTooltipTimer = setTimeout( () => {
-                    $tooltip.tooltip( 'close' );
-                }, settings.hide.duration );
+				window.CTCTCloseTooltipTimer = setTimeout( () => {
+					$tooltip.tooltip( 'close' );
+				}, settings.hide.duration );
 
-                e.stopImmediatePropagation();
-            } );
+				e.stopImmediatePropagation();
+			} );
 
-           app.allowMouseHoverOnTooltips();
-        } );
-    };
+			app.allowMouseHoverOnTooltips();
+		} );
+	};
 
 	app.init();
 
