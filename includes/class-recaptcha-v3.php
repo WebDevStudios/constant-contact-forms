ce43b8b7ee73d6645c5c3ec297ac67f7016e2226
Michael Beckwith <tw2113@gmail.com>
2019-08-23 22:24:45 -0500

    mostly empty class to future house version 3

---
+++ includes/class-recaptcha-v3.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Google reCAPTCHA v2.
+ *
+ * AKA "I am human" checkbox.
+ *
+ * @package    ConstantContact
+ * @subpackage reCAPTCHA
+ * @author     Constant Contact
+ * @since      NEXT
+ * phpcs:disable WebDevStudios.All.RequireAuthor -- Don't require author tag in docblocks.
+ */
+
+/**
+ * Class ConstantContact_reCAPTCHA_v2
+ *
+ * @since NEXT
+ */
+class ConstantContact_reCAPTCHA_v3 extends ConstantContact_reCAPTCHA {
+
+	/**
+	 * ConstantContact_reCAPTCHA_v2 constructor.
+	 *
+	 * @since NEXT
+	 *
+	 * @param string               $site_key   API v2 site key.
+	 * @param string               $secret_key API v2 secret key.
+	 * @param \ReCaptcha\ReCaptcha $recaptcha  reCAPTCHA object.
+	 */
+	public function __construct( $site_key, $secret_key, $recaptcha ) {
+		$this->site_key   = $site_key;
+		$this->secret_key = $secret_key;
+
+		parent::__construct( $recaptcha );
+	}
+}
