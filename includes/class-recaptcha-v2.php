27d1499a646628c59b8e7f0c70eb714247cde00b
Michael Beckwith <tw2113@gmail.com>
2019-08-27 14:36:15 -0500

    remove our key setting from the constructor

--- includes/class-recaptcha-v2.php
+++ includes/class-recaptcha-v2.php
@@ -26,10 +26,7 @@ class ConstantContact_reCAPTCHA_v2 extends ConstantContact_reCAPTCHA {
 	 * @param string               $secret_key API v2 secret key.
 	 * @param \ReCaptcha\ReCaptcha $recaptcha  reCAPTCHA object.
 	 */
-	public function __construct( $site_key, $secret_key, $recaptcha = '' ) {
-		$this->site_key   = $site_key;
-		$this->secret_key = $secret_key;
-
+	public function __construct( $recaptcha = '' ) {
 		if ( $recaptcha ) {
 			parent::__construct( $recaptcha );
 		}
