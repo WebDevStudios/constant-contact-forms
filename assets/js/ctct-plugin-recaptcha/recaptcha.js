7c08f25a81ec48f8513e4b252d2d8747204f1966
Michael Beckwith <tw2113@gmail.com>
2019-11-07 22:01:42 -0600

    some cleanup and removal of nested .ready()

--- assets/js/ctct-plugin-recaptcha/recaptcha.js
+++ assets/js/ctct-plugin-recaptcha/recaptcha.js
@@ -1,7 +1,5 @@
-jQuery(document).ready(function($) {
-	grecaptcha.ready(function () {
-		grecaptcha.execute(recaptchav3.site_key, {action: 'constantcontactsubmit'}).then(function (token) {
-			$('.ctct-form-wrapper form').append('<input type="hidden" name="g-recaptcha-response" value="' + token + '">');
-		});
+grecaptcha.ready(function () {
+	grecaptcha.execute( recaptchav3.site_key, {action: 'constantcontactsubmit'} ).then( function ( token ) {
+		jQuery( '.ctct-form-wrapper form' ).append( '<input type="hidden" name="g-recaptcha-response" value="' + token + '">' );
 	});
 });
