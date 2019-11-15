70bcc748c7bb6a80b0eab3b7fd879fdceb6cb8d2
Michael Beckwith <tw2113@gmail.com>
2019-11-15 14:17:02 -0600

    un-nest if statement and add return phpdoc

--- includes/class-recaptcha-v3.php
+++ includes/class-recaptcha-v3.php
@@ -22,29 +22,33 @@ class ConstantContact_reCAPTCHA_v3 extends ConstantContact_reCAPTCHA {
 	 * Enqueue our needed scripts.
 	 *
 	 * @since 1.7.0
+	 *
+	 * @return null
 	 */
 	public function enqueue_scripts() {
 		$this->set_recaptcha_keys();
 
-		if ( ConstantContact_reCAPTCHA::has_recaptcha_keys() ) {
-			$debug  = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG === true );
-			$suffix = ( true === $debug ) ? '' : '.min';
-
-			wp_enqueue_script(
-				'recaptcha-lib',
-				"//www.google.com/recaptcha/api.js?render={$this->site_key}",
-				[],
-				Constant_Contact::VERSION,
-				true
-			);
-			wp_enqueue_script(
-				'recaptcha-v3',
-				constant_contact()->url() . "assets/js/ctct-plugin-recaptcha{$suffix}.js",
-				[ 'jquery', 'recaptcha-lib' ],
-				Constant_Contact::VERSION,
-				true
-			);
-			wp_add_inline_script( 'recaptcha-v3', "recaptchav3 = {\"site_key\":\"{$this->site_key}\"}" );
+		if ( ! ConstantContact_reCAPTCHA::has_recaptcha_keys() ) {
+			return;
 		}
+
+		$debug  = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG === true );
+		$suffix = ( true === $debug ) ? '' : '.min';
+
+		wp_enqueue_script(
+			'recaptcha-lib',
+			"//www.google.com/recaptcha/api.js?render={$this->site_key}",
+			[],
+			Constant_Contact::VERSION,
+			true
+		);
+		wp_enqueue_script(
+			'recaptcha-v3',
+			constant_contact()->url() . "assets/js/ctct-plugin-recaptcha{$suffix}.js",
+			[ 'jquery', 'recaptcha-lib' ],
+			Constant_Contact::VERSION,
+			true
+		);
+		wp_add_inline_script( 'recaptcha-v3', "recaptchav3 = {\"site_key\":\"{$this->site_key}\"}" );
 	}
 }
