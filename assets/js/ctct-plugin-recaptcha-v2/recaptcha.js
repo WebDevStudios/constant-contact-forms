b3bc2316a4fb35c4cbe0f30b162499181fe87792
ravewebdev <36422618+ravewebdev@users.noreply.github.com>
2020-06-24 13:01:01 -0600

    CC-162: Replace Frontend Form Element IDs with Classes (#481)
    Amend ID usage in our frontend forms.

--- assets/js/ctct-plugin-recaptcha-v2/recaptcha.js
+++ assets/js/ctct-plugin-recaptcha-v2/recaptcha.js
@@ -1,24 +1,42 @@
-var ctctEnableBtn = function( index ) {
-    jQuery( jQuery( ".ctct-submit" )[ index ] ).attr( "disabled", false );
+/**
+ * Enable submit button.
+ *
+ * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @since  1.8.3
+ *
+ * @param  {Object} submitBtn Submit DOM element.
+ */
+var ctctEnableBtn = function( submitBtn ) {
+    jQuery( submitBtn ).attr( "disabled", false );
 }
 window.ctctEnableBtn = ctctEnableBtn;
 
-var ctctDisableBtn = function( index ) {
-    jQuery( jQuery( ".ctct-submit" )[ index ] ).attr( "disabled", "disabled" );
+/**
+ * Disable submit button.
+ *
+ * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @since  1.8.3
+ *
+ * @param  {Object} submitBtn Submit DOM element.
+ */
+var ctctDisableBtn = function( submitBtn ) {
+    jQuery( submitBtn ).attr( "disabled", "disabled" );
 }
 window.ctctDisableBtn = ctctDisableBtn;
 
 var renderReCaptcha = function() {
     jQuery( '.g-recaptcha' ).each( function( index, el ) {
+        const submitBtn = jQuery( el ).siblings( '.ctct-form-field-submit' ).find( '.ctct-submit' );
+
         grecaptcha.render( el, {
             'sitekey': jQuery( el ).attr( 'data-sitekey' ),
             'size': jQuery( el ).attr( 'data-size' ),
             'tabindex': jQuery( el ).attr( 'data-tabindex' ),
             'callback': function() {
-                window.ctctEnableBtn( index );
+                window.ctctEnableBtn( submitBtn );
             },
             'expired-callback': function() {
-                window.ctctDisableBtn( index );
+                window.ctctDisableBtn( submitBtn );
             },
             'isolated': true,
         } );
