91be5fabdb6f8de2caa0cb7e00b96690b8ed8011
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2020-08-07 13:05:30 -0600

    Ignore all prefix linting errors in deprecated file
    CC-178

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -7,6 +7,7 @@
  * @since   NEXT
  *
  * phpcs:disable WebDevStudios.All.RequireAuthor -- Don't require author tag in docblocks.
+ * phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- Incorrectly prefixed functions have been deprecated.
  */
 
 /**
@@ -20,7 +21,7 @@
  * @param  string $default Default value if no option exists.
  * @return mixed           Option value.
  */
-function ctct_get_settings_option( $key = '', $default = null ) { // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- Function is prefixed.
+function ctct_get_settings_option( $key = '', $default = null ) {
 	_deprecated_function( __FUNCTION__, 'NEXT', 'constant_contact_get_option' );
 
 	return constant_contact_get_option( $key, $default );
