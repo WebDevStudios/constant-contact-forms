5bf6996cef917bc719ae2015351143ebfbe874d2
Richard Aber <richard.aber@webdevstudios.com>
2021-10-04 19:36:06 -0500

    CC-266 Add tests to ensure that constant_contact_set_has_exceptions can set ctct_exceptions_exist option to string 'false' and string 'true'

--- tests/test-helper-functions.php
+++ tests/test-helper-functions.php
@@ -33,4 +33,40 @@ class ConstantContact_Helper_Functions_Test extends WP_UnitTestCase {
 		delete_option( self::EXCEPT_OPTION_NAME );
 		parent::tearDown();
 	}
+
+	/**
+	 * Test that constant_contact_set_has_exceptions( 'false' )
+	 * sets the ctct_exceptions_exist option to 'false'.
+	 *
+	 * @since NEXT
+	 *
+	 * @test
+	 */
+	public function test_constant_contact_set_has_exceptions_false() {
+
+		constant_contact_set_has_exceptions( 'false' );
+
+		$this->assertEquals(
+			get_option( self::EXCEPT_OPTION_NAME ),
+			'false'
+		);
+	}
+
+	/**
+	 * Test that constant_contact_set_has_exceptions( 'true' )
+	 * sets the ctct_exceptions_exist option to 'true'.
+	 *
+	 * @since NEXT
+	 *
+	 * @test
+	 */
+	public function test_constant_contact_set_has_exceptions_true() {
+
+		constant_contact_set_has_exceptions( 'true' );
+
+		$this->assertEquals(
+			get_option( self::EXCEPT_OPTION_NAME ),
+			'true'
+		);
+	}
 }
