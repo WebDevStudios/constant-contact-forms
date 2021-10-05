87b2abc05d44706caa9b57a0bf0a89eacb272779
Richard Aber <richard.aber@webdevstudios.com>
2021-10-04 19:32:55 -0500

    CC-266 Add setUp and tearDown methods

--- tests/test-helper-functions.php
+++ tests/test-helper-functions.php
@@ -8,4 +8,23 @@
 
 class ConstantContact_Helper_Functions_Test extends WP_UnitTestCase {
 
+	/**
+	 * Runs the routine before each test is executed.
+	 *
+	 * @since NEXT
+	 */
+	public function setUp() {
+		// This code will run before each test.
+		parent::setUp();
+	}
+
+	/**
+	 * After a test method runs, resets any state in WordPress the test method might have changed.
+	 *
+	 * @since NEXT
+	 */
+	public function tearDown() {
+		// This code will run after each test.
+		parent::tearDown();
+	}
 }
