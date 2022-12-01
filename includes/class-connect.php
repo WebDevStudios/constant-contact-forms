263ceef370c3031586aa70e67988055a2e0f5017
David Romsey <david.romsey@webdevstudios.com>
2022-12-01 10:50:53 -0500

    Remove additional options when disconnecting

--- includes/class-connect.php
+++ includes/class-connect.php
@@ -346,6 +346,12 @@ class ConstantContact_Connect {
 			delete_option( 'ctct_refresh_token' );
 			delete_option( '_ctct_refresh_token' );
 			delete_option( '_ctct_expires_in' );
+
+			delete_option( 'CtctConstantContactcode_verifier' );
+			delete_option( 'CtctConstantContactState' );
+			delete_option( 'ctct_auth_url' );
+			delete_option( 'ctct_key' );
+
 			constant_contact_delete_option( '_ctct_form_state_authcode' );
 
 			wp_clear_scheduled_hook( 'refresh_token_job' );
