172223c19679121ea1c37d386426c08b4447bc6f
Michael Beckwith <tw2113@gmail.com>
2023-01-24 13:15:40 -0600

    quick misc alignment

--- includes/class-api.php
+++ includes/class-api.php
@@ -735,7 +735,7 @@ class ConstantContact_API {
 
 		$contact = new Contact();
 
-		$contact->email_address = new stdClass();
+		$contact->email_address          = new stdClass();
 		$contact->email_address->address = sanitize_text_field( $email );
 
 		$this->add_to_list( $contact, $list );
@@ -813,10 +813,10 @@ class ConstantContact_API {
 					// Do nothing, as we already captured.
 					break;
 				case 'phone_number':
-					$phone_number = new stdClass();
+					$phone_number               = new stdClass();
 					$phone_number->phone_number = $value;
-					$phone_number->kind = 'home';
-					$contact->phone_numbers[] = $phone_number;
+					$phone_number->kind         = 'home';
+					$contact->phone_numbers[]   = $phone_number;
 					break;
 				case 'company':
 					$contact->company_name = $value;
