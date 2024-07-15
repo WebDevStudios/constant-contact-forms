649bce6e22746ad23d8208703a1f43efb3573651
Michael Beckwith <tw2113@gmail.com>
2024-07-15 11:09:40 -0500

    touch up our styles

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -941,42 +941,50 @@ function ctct_modal_script_styles() {
 		return;
 	}
 ?>
-	<style>
-		.modal {
-			display: none;
-			position: fixed;
-			z-index: 1;
-			left: 0;
-			top: 0;
-			width: 100%;
-			height: 100%;
-			overflow: auto;
-			background-color: rgb(0, 0, 0);
-			background-color: rgba(0, 0, 0, 0.4);
-		}
+<style>
+	.modal {
+		display: block;
+		position: fixed;
+		z-index: 1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: rgb(0, 0, 0);
+		background-color: rgba(0, 0, 0, 0.4);
+	}
 
-		.modal-content {
-			background-color: #fefefe;
-			margin: 15% auto;
-			padding: 20px;
-			border: 1px solid #888;
-			width: 50%;
-		}
+	.modal-content {
+		border-left: solid #ff9e1a 5px;
+		background-color: #fefefe;
+		margin: 15% auto;
+		padding: 20px;
+		width: 50%;
+		position: relative;
+	}
+	.modal-content a {
+		color: #144bee;
+	}
 
-		.ctct-feedback-close {
-			color: #aaa;
-			float: right;
-			font-size: 28px;
-			font-weight: bold;
-		}
+	.modal-content #ctct-skip-deactivate {
+		font-size: 16px;
+	}
 
-		.ctct-feedback-close:hover,
-		.ctct-feedback-close:focus {
-			color: black;
-			text-decoration: none;
-			cursor: pointer;
-		}
-	</style>
+	.modal-content img {
+		position: absolute;
+		bottom: 20px;
+		right: 20px;
+		width: 50px;
+	}
+
+	.ctct-feedback-close {
+		float: right;
+		font-size: 28px;
+		font-weight: bold;
+		text-decoration: none;
+	}
+</style>
 
 <script>
 	window.addEventListener('load', function () {
