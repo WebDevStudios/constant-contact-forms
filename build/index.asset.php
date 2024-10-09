7f745970cf999c792f4c6c725b571c1a507c9e75
Michael Beckwith <tw2113@gmail.com>
2024-10-09 13:40:06 -0500

    build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e8e0b41bdd8aa33b742c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '93223449ec663b0ce3d3');
