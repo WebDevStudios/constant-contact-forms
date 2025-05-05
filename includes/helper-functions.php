f53963c15ca5cb9ae7ee3592867592a8423c775e
Michael Beckwith <tw2113@gmail.com>
2025-05-05 13:39:56 -0500

    Revert "list screen already handled by is_constant_contact(), only show notif if we have the elevated exception"
    This reverts commit a1f4cb0403cb5ce622df05f687909edcc66be972.

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -991,5 +991,10 @@ function constant_contact_maybe_show_list_notes_notification() :bool {
 		return false;
 	}
 
-	return constant_contact_get_has_exceptions();
+	$screen = get_current_screen();
+	if ( is_null( $screen ) || 'edit-ctct_lists' !== $screen->id ) {
+		return false;
+	}
+
+	return true;
 }
