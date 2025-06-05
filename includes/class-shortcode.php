52ce02fca7008755d8e402d000eeb83808ed1c2f
Michael Beckwith <tw2113@gmail.com>
2025-06-05 16:10:20 -0500

    fill in intended version, document parameters and set param types

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -80,9 +80,12 @@ class ConstantContact_Shortcode {
 	 * Additional cache invalidation for form list transient; runs on save_post.
 	 *
 	 * @since 1.6.0
-	 * @since NEXT added method parameters.
+	 * @since 2.11.0 added method parameters.
+	 *
+	 * @param int     $post_id Post ID.
+	 * @param WP_Post $post    Post object.
 	 */
-	public function clear_forms_list_transient( $post_id, $post ) {
+	public function clear_forms_list_transient( int $post_id, WP_Post $post ) {
 		if ( ! in_array( $post->post_type, [ 'ctct_forms', 'ctct_lists' ], true ) ) {
 			return;
 		}
