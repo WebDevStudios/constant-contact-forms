3998f982cc360d768e485fafe5f0b4435a81ccc9
Michael Beckwith <tw2113@gmail.com>
2025-07-14 11:44:24 -0500

    build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a5481f6d0a5a9a9f8c30');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '18f4155c38e7e7f7ccbc');
