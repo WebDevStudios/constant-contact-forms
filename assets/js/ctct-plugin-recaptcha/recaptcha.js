e0cc24f8f74747c384e8f324712433c2576ec676
Michael Beckwith <tw2113@gmail.com>
2025-08-01 16:48:39 -0500

    begin attempting to attach Google responses on submit to better avoid timeouts

--- assets/js/ctct-plugin-recaptcha/recaptcha.js
+++ assets/js/ctct-plugin-recaptcha/recaptcha.js
@@ -1,13 +1,25 @@
 grecaptcha.ready(function () {
-	grecaptcha.execute( recaptchav3.site_key, {action: 'constantcontactsubmit'} ).then( function ( token ) {
-		let forms = document.querySelectorAll( '.ctct-form-wrapper form' );
-		let recaptchaResponse = document.createElement('input');
-		recaptchaResponse.setAttribute('type', 'hidden');
-		recaptchaResponse.setAttribute('name', 'g-recaptcha-response');
-		recaptchaResponse.setAttribute('value', token);
+	let forms = document.querySelectorAll('.ctct-form-wrapper form');
+	Array.from(forms).forEach(function (form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+			debugger;
+			try {
+				grecaptcha.execute(recaptchav3.site_key, {action: 'constantcontactsubmit'}).then(function (token) {
+					let recaptchaResponse = document.createElement('input');
+					recaptchaResponse.setAttribute('type', 'hidden');
+					recaptchaResponse.setAttribute('name', 'g-recaptcha-response');
+					recaptchaResponse.setAttribute('value', token);
 
-		Array.from( forms ).forEach( function( form ) {
-			form.append(recaptchaResponse.cloneNode(true));
-		} );
+					form.append(recaptchaResponse.cloneNode(true));
+					debugger;
+					form.submit();
+				});
+			} catch (error) {
+				debugger;
+				console.log(error);
+				return false;
+			}
+		});
 	});
 });
