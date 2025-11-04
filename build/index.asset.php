e3df96ad4d698914b0824139c27062a47a183088
Michael Beckwith <tw2113@gmail.com>
2025-11-04 13:43:23 -0600

    update block styles for anniversary/birthday dates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => '38f405b72e0528cfb8ba');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render', 'wp-url'), 'version' => 'd83f4ba63e0c7029de08');
