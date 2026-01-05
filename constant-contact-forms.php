5f28e6948d95b62d2184f865bb8d87c9d2352afe
Michael Beckwith <tw2113@gmail.com>
2026-01-05 12:15:29 -0600

    version bumps, current changelog

--- constant-contact-forms.php
+++ constant-contact-forms.php
@@ -12,7 +12,7 @@
  * Plugin Name: Constant Contact Forms for WordPress
  * Plugin URI:  https://www.constantcontact.com
  * Description: Be a better marketer. All it takes is Constant Contact email marketing.
- * Version:     2.15.2
+ * Version:     2.16.0
  * Author:      Constant Contact
  * Author URI:  https://www.constantcontact.com/index?pn=miwordpress
  * Requires PHP: 8.1
@@ -75,7 +75,7 @@ class Constant_Contact {
 	 * @since 1.0.0
 	 * @var string
 	 */
-	const VERSION = '2.15.2';
+	const VERSION = '2.16.0';
 
 	/**
 	 * URL of plugin directory.
