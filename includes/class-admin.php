b5fbf191e53e88ffa3a788ac439c6526e452aa57
Michael Beckwith <tw2113@gmail.com>
2026-05-04 11:14:52 -0500

    make use of full url instead of relative

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -215,7 +215,7 @@ class ConstantContact_Admin {
 					echo wp_kses( '</ul>', [ 'ul' => [] ] );
 				}
 				?>
-				<a href="edit.php?post_type=ctct_forms&page=ctct_options_connect" class="ctct-status ctct-<?php echo esc_attr( $api_status ); ?>" title="<?php echo esc_attr( $connect_alt ); ?>">
+				<a href="<?php echo esc_url( admin_url( 'edit.php?post_type=ctct_forms&page=ctct_options_connect' ) ); ?>" class="ctct-status ctct-<?php echo esc_attr( $api_status ); ?>" title="<?php echo esc_attr( $connect_alt ); ?>">
 					<?php echo esc_html( $connect_title ); ?>
 				</a>
 			</div>
