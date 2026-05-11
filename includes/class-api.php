e3a470ac3416fd20576bda3e5f18d5a92a35e916
Michael Beckwith <tw2113@gmail.com>
2026-05-11 13:47:03 -0500

    numbered placeholder, use getMessage method

--- includes/class-api.php
+++ includes/class-api.php
@@ -1315,7 +1315,7 @@ class ConstantContact_API {
 		} catch ( Exception $ex ) {
 			$error                = new stdClass();
 			$error->error_key     = get_class( $ex );
-			$error->error_message = $ex->xdebug_message;
+			$error->error_message = $ex->getMessage();
 
 			add_filter( 'constant_contact_force_logging', '__return_true' );
 			constant_contact_forms_maybe_set_exception_notice( $ex );
@@ -1670,7 +1670,7 @@ class ConstantContact_API {
 		$content = sprintf(
 			$content,
 			sprintf(
-				'<a href="%s">',
+				'<a href="%1$s">',
 				get_bloginfo( 'url' )
 			),
 			$title,
