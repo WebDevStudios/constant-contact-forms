9111da903d6ed44672e2aa934385ba120e4833f3
Michael Beckwith <tw2113@gmail.com>
2026-05-11 14:53:36 -0500

    the great str_contains switch

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -231,7 +231,7 @@ class ConstantContact_Settings {
 			return $file;
 		}
 
-		$plugin_page = false !== strpos( $plugin_page, $this->key ) ? "{$this->key}_general" : $plugin_page; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited -- OK overriding of WP global.
+		$plugin_page = str_contains( $plugin_page, $this->key ) ? "{$this->key}_general" : $plugin_page; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited -- OK overriding of WP global.
 
 		return $file;
 	}
@@ -1234,7 +1234,7 @@ class ConstantContact_Settings {
 		$value = trim( $value );
 
 		// Keys need to be under 50 chars long and have no spaces inside them.
-		if ( false !== strpos( $value, ' ' ) || 50 <= strlen( $value ) ) {
+		if ( str_contains( $value, ' ' ) || 50 <= strlen( $value ) ) {
 			return '';
 		}
 
@@ -1255,7 +1255,7 @@ class ConstantContact_Settings {
 		$value = trim( $value );
 
 		// Keys need to be under 50 chars long and have no spaces inside them.
-		if ( false !== strpos( $value, ' ' ) || 50 <= strlen( $value ) ) {
+		if ( str_contains( $value, ' ' ) || 50 <= strlen( $value ) ) {
 			return '';
 		}
 
