357784251bbc44a38cb18f6c3e9ad3409153961f
Michael Beckwith <tw2113@gmail.com>
2026-06-09 14:49:34 -0500

    make sure we clear our refreshing token option on uninstall too

--- includes/class-uninstall.php
+++ includes/class-uninstall.php
@@ -64,6 +64,7 @@ class ConstantContact_Uninstall {
 			'ctct_log_suffix',
 			'ctct_refresh_failures',
 			'ctct_account_domain_hash',
+			'ctct_refreshing_token',
 			Constant_Contact::$activated_date_option,
 			ConstantContact_Notifications::$dismissed_notices_option,
 			ConstantContact_Notifications::$review_dismissed_option,
