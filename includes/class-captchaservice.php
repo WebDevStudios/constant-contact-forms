e9e0661f16b454ad054c758a008c0b73caf99f0a
Michael Beckwith <tw2113@gmail.com>
2026-07-07 13:56:28 -0500

    spacing

--- includes/class-captchaservice.php
+++ includes/class-captchaservice.php
@@ -53,7 +53,7 @@ class ConstantContact_CaptchaService {
 	 *
 	 * @return string The captcha service in use.
 	 */
-	public function get_selected_captcha_service() : string {
+	public function get_selected_captcha_service(): string {
 		$settings_values = get_option( $this->plugin_settings_key );
 
 		return $settings_values[ $this->captcha_service_option_key ] ?? '';
@@ -67,7 +67,7 @@ class ConstantContact_CaptchaService {
 	 *
 	 * @return bool True if a captcha service is selected and keys are present, or false otherwise.
 	 */
-	public function is_captcha_enabled() : bool {
+	public function is_captcha_enabled(): bool {
 		$captcha_service = $this->get_selected_captcha_service();
 
 		// Bail early if the settings aren't available.
