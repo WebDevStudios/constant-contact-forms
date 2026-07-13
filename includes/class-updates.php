1edf14941f4f83940d59ed6c44a986e6224024d4
Michael Beckwith <tw2113@gmail.com>
2026-07-13 12:58:54 -0500

    phpcs while we are here

--- includes/class-updates.php
+++ includes/class-updates.php
@@ -42,7 +42,7 @@ class ConstantContact_Updates {
 	 *
 	 * @since 1.0.0
 	 */
-	public function hooks() {
+	public function hooks(): void {
 		if ( is_admin() ) {
 			add_action( 'plugins_loaded', [ $this, 'check_for_update_needed' ] );
 		}
@@ -54,7 +54,7 @@ class ConstantContact_Updates {
 	 *
 	 * @since 1.0.0
 	 */
-	public function check_for_update_needed() {
+	public function check_for_update_needed(): void {
 
 		$installed = get_option( 'ctct_plugin_version', '0.0.0' );
 		$current   = esc_attr( $this->plugin->version );
@@ -88,7 +88,7 @@ class ConstantContact_Updates {
 	 *
 	 * @param string $update_id Update key to use for version.
 	 */
-	public function add_notification( string $update_id ) {
+	public function add_notification( string $update_id ): void {
 
 		$current_notifs = get_option( 'ctct_update_notifications', [] );
 		$compare_notifs = $current_notifs;
@@ -118,7 +118,7 @@ class ConstantContact_Updates {
 	 *
 	 * @since 2.22.0
 	 */
-	public function run_update_v2_19_0_to_v2_22_0() {
+	public function run_update_v2_19_0_to_v2_22_0(): void {
 		update_option( 'ctct_account_domain_hash', '' );
 	}
 
@@ -129,7 +129,7 @@ class ConstantContact_Updates {
 	 *
 	 * @since 2.22.0
 	 */
-	public function run_update_v2_20_0_to_v2_22_0() {
+	public function run_update_v2_20_0_to_v2_22_0(): void {
 		update_option( 'ctct_account_domain_hash', '' );
 	}
 
@@ -140,7 +140,7 @@ class ConstantContact_Updates {
 	 *
 	 * @since 2.22.0
 	 */
-	public function run_update_v2_21_0_to_v2_22_0() {
+	public function run_update_v2_21_0_to_v2_22_0(): void {
 		update_option( 'ctct_account_domain_hash', '' );
 	}
 }
