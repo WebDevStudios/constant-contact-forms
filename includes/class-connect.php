6370ade15f69d3785d126982734fad12fa3275bc
Michael Beckwith <tw2113@gmail.com>
2026-07-13 09:03:08 -0500

    rename variable. Return original option value if decryption fails. this should prevent further disconnection issues

--- includes/class-connect.php
+++ includes/class-connect.php
@@ -468,25 +468,24 @@ class ConstantContact_Connect {
 		if ( $fallback_to_ctct_opt ) {
 			$options = get_option( 'ctct_options_settings', false );
 			if ( $options && isset( $options[ $check_key ] ) ) {
-				$encrypted_token = $options[ $check_key ];
+				$maybe_encrypted_token = $options[ $check_key ];
 			} else {
 				return false;
 			}
 		} else {
-			$encrypted_token = get_option( $check_key );
-			if ( ! $encrypted_token ) {
+			$maybe_encrypted_token = get_option( $check_key );
+			if ( ! $maybe_encrypted_token ) {
 				return false;
 			}
 		}
 
 		try {
-			$return = Crypto::decrypt( $encrypted_token, $key );
+			$return = Crypto::decrypt( $maybe_encrypted_token, $key );
 		} catch ( Exception $e ) {
-			$return = '';
+			$return = $maybe_encrypted_token;
 		}
 
 		return $return;
-
 	}
 
 	/**
